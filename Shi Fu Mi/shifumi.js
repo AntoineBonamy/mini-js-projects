698ceb38b4e00acaca101fff2bfb67eef569612c
Elindanova <bonamy.antoine@hotmail.fr>
2023-03-09 14:26:35 +0100

    remove death code from shifumi.js

--- Shi Fu Mi/shifumi.js
+++ Shi Fu Mi/shifumi.js
@@ -17,62 +17,6 @@ function cpPlay (cpChoice) {
     } 
 }
 
-/*function Shifumi() {
-    function cpPlay (cpChoice) {
-        if (cpChoice < 33)  {
-            console.log("computer a choisi Pierre");
-            return cpItem = "pierre";
-        }
-        else if (cpChoice < 67) {
-            console.log("computer a choisi Feuille");
-            return cpItem = "feuille";
-        }
-        else {
-            console.log("computer a choisi Ciseaux");
-            return cpItem = "ciseaux";
-        } 
-    }    
-    let playerChoice = prompt("pierre, feuille, ciseaux ?");
-    cpPlay(cpChoice);
-    if (playerChoice === "pierre") {
-        if (cpItem === "ciseaux") {
-            console.log("Gagné !");
-        }
-        else if (cpItem === "feuille") {
-            console.log("Perdu !");
-        }
-        else if (cpItem === "pierre") {
-            console.log("Egalité");
-        }
-    }
-    else if (playerChoice === "feuille") {
-        if (cpItem === "pierre") {
-            console.log("Gagné !");
-        }
-        else if (cpItem === "ciseaux") {
-            console.log("Perdu !");
-        }
-        else if (cpItem === "feuille"){
-            console.log("Egalité");
-        }
-    }
-    else if (playerChoice === "ciseaux") {
-        if (cpItem === "feuille") {
-            console.log("Gagné !");
-        }
-        else if (cpItem === "pierre") {
-            console.log("Perdu !");
-        }
-        else if (cpItem === "ciseaux"){
-            console.log("Egalité");
-        }
-    }
-    else {
-        console.log("Réponse invalide");
-    }
-}
-*/
-
 cpPlay(cpChoice);
 
 function pierrePlayer() {
